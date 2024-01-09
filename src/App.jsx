b19c08bdd169b74289a0651d7eb4721d3ce171cb
nugeulous <c.ryan.nugent@gmail.com>
2024-01-08 21:27:07 -0500

    Completed adding data.js and using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { puppyList } from './data.js'
+
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
 
-
-  return (
-    <>
+   return (
       <div>
-        
+        { 
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
-    </>
-  )
+   );
 }
 
 export default App
